b6d8c22afaa4f8631ea4c103426224bf006df833
Edoardo <edoardo.malfatto@hotmail.it>
2021-02-25 15:15:15 +0100

    completato(?)

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero 
+//e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// 
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+//se il numero è divisibile per 5 e tre allora scrivi frizzbuz, 
+//se è divisibile per 5 scrivi buzz,se divisibile per tre scrivi frizz
+
+for (i=0; i<100 ; i++) {
+    var numeroAumentato = i+1;
+
+    if (numeroAumentato % 5 == 0 && numeroAumentato % 3 == 0) {
+        numeroAumentato=("FizzBuzz");   
+        console.log(numeroAumentato);
+ 
+    } else if (numeroAumentato % 5 == 0) {
+        numeroAumentato=("Buzz");
+        console.log(numeroAumentato);
+
+    }else if (numeroAumentato % 3 == 0) {
+        numeroAumentato=("Fizz"); 
+        console.log(numeroAumentato);
+
+    }else if (numeroAumentato % 5 != 0 || numeroAumentato % 3 != 0){
+        console.log(numeroAumentato);
+    }     
+}
+
+
